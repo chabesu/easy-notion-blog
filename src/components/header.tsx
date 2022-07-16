1da57c5ed04d7413abf513167bb6a5fc8c0c6cc6
chabesu <chabesublog@gmail.com>
2022-07-16 18:37:28 +0900

    Add portfolio

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,6 +15,7 @@ const Header = () => {
   const navItems: NavItem[] = [
     { label: 'Home', path: '/' },
     { label: 'Blog', path: '/blog' },
+    { label: 'Portfolio', path: 'https://notion-portfolio-site.vercel.app/' },
   ]
 
   return (
