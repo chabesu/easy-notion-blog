bb07818e1ad621539ab9ad9922685111bb496041
otoyo <hiroki.t.1988@gmail.com>
2022-07-23 20:36:35 +0900

    Build equation

--- src/lib/notion/client.ts
+++ src/lib/notion/client.ts
@@ -11,6 +11,7 @@ import {
   Image,
   Code,
   Quote,
+  Equation,
   Callout,
   Embed,
   Video,
@@ -464,6 +465,13 @@ export async function getAllBlocksByBlockId(blockId) {
 
           block.Quote = quote
           break
+        case 'equation':
+          const equation: Equation = {
+            Expression: item[item.type].expression,
+          }
+
+          block.Equation = equation
+          break
         case 'callout':
           const callout: Callout = {
             RichTexts: item[item.type].rich_text.map(_buildRichText),
@@ -649,11 +657,6 @@ function _buildPost(data) {
 }
 
 function _buildRichText(item) {
-  const text: Text = {
-    Content: item.text.content,
-    Link: item.text.link,
-  }
-
   const annotation: Annotation = {
     Bold: item.annotations.bold,
     Italic: item.annotations.italic,
@@ -664,10 +667,22 @@ function _buildRichText(item) {
   }
 
   const richText: RichText = {
-    Text: text,
     Annotation: annotation,
     PlainText: item.plain_text,
     Href: item.href,
+  }
+
+  if (item.type === 'text') {
+    const text: Text = {
+      Content: item.text.content,
+      Link: item.text.link,
+    }
+    richText.Text = text
+  } else if (item.type === 'equation') {
+    const equation: Equation = {
+      Expression: item.equation.expression,
+    }
+    richText.Equation = equation
   }
 
   return richText
