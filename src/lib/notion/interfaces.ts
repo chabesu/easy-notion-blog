b8ee26af991fe9ddb9f430bdf2e620292cf11c39
otoyo <hiroki.t.1988@gmail.com>
2022-08-26 18:06:04 +0900

    Define interfaces for ColumnList

--- src/lib/notion/interfaces.ts
+++ src/lib/notion/interfaces.ts
@@ -31,6 +31,7 @@ export interface Block {
   Bookmark?: Bookmark
   LinkPreview?: LinkPreview
   Table?: Table
+  ColumnList?: ColumnList
 }
 
 export interface Paragraph {
@@ -138,6 +139,17 @@ export interface TableRow {
 
 export interface TableCell {
   RichTexts: RichText[]
+}
+
+export interface ColumnList {
+  Columns: Column[]
+}
+
+export interface Column {
+  Id: string
+  Type: string
+  HasChildren: boolean
+  Children: Block[]
 }
 
 export interface List {
