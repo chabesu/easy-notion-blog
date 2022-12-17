2d4a3c958c4a9c48085dea6e30c348323f1b69b7
chabesu <chabesublog@gmail.com>
2022-12-17 18:35:39 +0900

    Fix header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-
-import { SITE_TITLE } from './document-head'
-import styles from '../styles/header.module.css'
-
-interface NavItem {
-  label: string
-  path: string
-}
-
-const Header = () => {
-  const { asPath } = useRouter()
-
-  const navItems: NavItem[] = [
-    { label: 'Home', path: '/' },
-    { label: 'Blog', path: '/blog' },
-    { label: 'Portfolio', path: 'https://notion-portfolio-site.vercel.app/' },
-    { label: 'Contact', path: '/contact' },
-  ]
-
-  return (
-    <header className={styles.header}>
-      <h1>
-        <Link href="/" passHref>
-          <a>{SITE_TITLE}</a>
-        </Link>
-      </h1>
-
-      <ul>
-        {navItems.map(({ label, path }) => (
-          <li key={label}>
-            <Link href={path} passHref>
-              <a className={asPath === path ? 'active' : null}>{label}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </header>
-  )
-}
-
-export default Header
-=======
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
@@ -84,4 +39,3 @@ const Header = () => {
 }
 
 export default Header
->>>>>>> e5669f8 (Replace SITE_TITLE and SITE_DESCRIPTION with NEXT_PUBLIC_)
