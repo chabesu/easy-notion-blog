a1d591aa72bb2ac6a0498ea7a525a9e00635ee21
chabesu <chabesublog@gmail.com>
2022-12-19 00:17:31 +0900

    Delete LikeButton

--- lib/notion/client.ts
+++ lib/notion/client.ts
@@ -757,21 +757,6 @@ export async function getAllTags(): Promise<string[]> {
     .sort()
 }
 
-export async function incrementLikes(post: Post) {
-  const result = await client.pages.update({
-    page_id: post.PageId,
-    properties: {
-      Like: (post.Like || 0) + 1,
-    },
-  })
-
-  if (!result) {
-    return null
-  }
-
-  return _buildPost(result)
-}
-
 function _buildFilter(conditions = []) {
   if (process.env.NODE_ENV === 'development') {
     return { and: conditions }
@@ -834,8 +819,6 @@ function _buildPost(pageObject: responses.PageObject): Post {
     OGImage:
       prop.OGImage.files.length > 0 ? prop.OGImage.files[0].file.url : null,
     Rank: prop.Rank.number,
-    // Like: prop.Like != undefined ? prop.Like.number : 0,
-    Like: prop.Like.number,
   }
 
   return post
